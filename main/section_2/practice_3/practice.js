3d72f099d5b4cf54ca693cc8346b69fca42b8c18
tyh <756467256@qq.com>
2020-01-01 16:15:02 +0800

    {YunHan}:finish section_2

--- main/section_2/practice_3/practice.js
+++ main/section_2/practice_3/practice.js
@@ -1,5 +1,24 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var count_obj = {};
+  for (var i = 0, len = collection.length; i < len; i++) {
+    if (collection[i].length === 1 && count_obj[collection[i]]) {
+      count_obj[collection[i]]++;
+    } else if (collection[i].length !== 1 && count_obj[collection[i]]) {
+      count_obj[collection[i]] += parseInt(collection[i].match(/\d+/g));
+    } else if (collection[i].length === 1 && !count_obj[collection[i]]) {
+      count_obj[collection[i]] = 1;
+    } else {
+      count_obj[collection[i]] = parseInt(collection[i].match(/\d+/g));
+    } 
+  var result = [];
+  for (var i in count_obj) {
+    var collection_count = {};
+    collection_count.key = i;
+    collection_count.count = count_obj[i];
+    result.push(collection_count)
+  }
+  return result;
 }
 
 module.exports = count_same_elements;
