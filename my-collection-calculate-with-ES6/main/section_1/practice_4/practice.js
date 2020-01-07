5389085580c08ffa8b013c58400e3f3b7007ea3e
tyh <756467256@qq.com>
2020-01-07 13:08:02 +0800

    {YunHan}{feat}--{ES6}:finish section_1 practice_4

--- my-collection-calculate-with-ES6/main/section_1/practice_4/practice.js
+++ my-collection-calculate-with-ES6/main/section_1/practice_4/practice.js
@@ -1,24 +1,9 @@
+var get_intersection = require('../../../main/collectionOperator/get_intersection.js');
+
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
-  var collection_a_arr = [];
-  for (var i = 0, len = collection_a.length; i < len; i++) {
-    collection_a_arr.push(collection_a[i].key);
-  }
-  var collection_b_arr = object_b.value;
-  var collection = collection_b_arr.concat(collection_a_arr);
-  var count = {};
-  var result = [];
-  for (var i = 0, len = collection_b_arr.length; i < len; i++) {
-    if (!count[collection_b_arr[i]]) {
-      count[collection[i]] = 1;
-    }
-  }
-  for (var i = 0, len = collection_a_arr.length; i < len; i++) {
-    if (count[collection_a_arr[i]]) {
-      result.push(collection_a_arr[i])
-    }
-  }
-  return result;
+  let a = [];
+  collection_a.forEach(element => a.push(element.key));
+  return get_intersection(object_b.value, a);
 }
 
 module.exports = collect_same_elements;
