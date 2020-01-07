2d2ff10479ecabeae17f05d85c0c765ac1f2d947
tyh <756467256@qq.com>
2020-01-07 10:39:01 +0800

    {YunHan}{feat}--{ES6}:finish reduce compare_collections

--- my-collection-calculate-with-ES6/main/reduce/compare_collections.js
+++ my-collection-calculate-with-ES6/main/reduce/compare_collections.js
@@ -1,33 +1,16 @@
 'use strict';
-
+var rank_desc = require('../../main/map/rank_desc.js');
 function compare_collections(collection_a, collection_b) {
-  var rank_desc = function(collection){
-    if (collection.length <= 1) { 
-      return collection; 
-    }
-    var pivotIndex = Math.floor(collection.length / 2);
-    var pivot = collection[pivotIndex];
-    var left = [];
-    var right = [];
-    for (var i = 0; i < collection.length; i++) {
-      if (i != pivotIndex) {
-        if (collection[i] < pivot) {
-          left.push(collection[i]);
-        } else {
-          right.push(collection[i]);
-        }
-      }
-    }
-    return rank_desc(left).concat([pivot], rank_desc(right));
-  };
   collection_a = rank_desc(collection_a);
   collection_b = rank_desc(collection_b);
-  for (var i = 0, len = collection_b.length; i < len; i++) {
-    if (collection_b[i] !== collection_a[i]) {
-      return false;
-    }
-  }
-  return true;
+  collection_a.forEach(element_a => {
+    collection_b.forEach(element_b => {
+      if (element_a !== element_b) {
+        return false
+      }
+    })
+  })
+  return true
 }
 
 module.exports = compare_collections;
