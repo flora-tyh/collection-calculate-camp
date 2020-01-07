40ba2b1a2baef9382009bf77237870a14fbaf99b
tyh <756467256@qq.com>
2020-01-07 12:08:25 +0800

    {YunHan}{feat}--{ES6}:finish reduce find_element_last_subscript

--- my-collection-calculate-with-ES6/main/reduce/find_element_last_subscript.js
+++ my-collection-calculate-with-ES6/main/reduce/find_element_last_subscript.js
@@ -1,11 +1,7 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-  for (var i = collection.length - 1; i > 0; i--) {
-    if (element === collection[i]) {
-      return i;
-    }
-  }
+  return collection.lastIndexOf(element);
 }
 
 module.exports = calculate_elements_sum;
