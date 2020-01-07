92017952a2ddb9e2344240c4208a2ef3ff2ee271
tyh <756467256@qq.com>
2020-01-07 10:54:14 +0800

    {YunHan}{feat}--{ES6}:finish reduce compute_chain_median

--- my-collection-calculate-with-ES6/main/reduce/compute_chain_median.js
+++ my-collection-calculate-with-ES6/main/reduce/compute_chain_median.js
@@ -1,26 +1,7 @@
 'use strict';
-
+var rank_desc = require('../../main/map/rank_desc.js');
 function compute_chain_median(collection) {
   var arr = collection.split("->");
-  var rank_desc = function(collection){
-    if (collection.length <= 1) { 
-      return collection; 
-    }
-    var pivotIndex = Math.floor(collection.length / 2);
-    var pivot = +collection[pivotIndex];
-    var left = [];
-    var right = [];
-    for (var i = 0; i < collection.length; i++) {
-      if (i != pivotIndex) {
-        if (collection[i] < pivot) {
-          left.push(+collection[i]);
-        } else {
-          right.push(+collection[i]);
-        }
-      }
-    }
-    return rank_desc(left).concat([pivot], rank_desc(right));
-  };
   arr = rank_desc(arr);
   var len = arr.length;
   if (len % 2) {
