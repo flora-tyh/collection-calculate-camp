4be576287dde94764704a5267bcdd06dff9a8097
tyh <756467256@qq.com>
2020-01-07 12:04:56 +0800

    {YunHan}{fixed}--{ES6}: reduce compute_median

--- my-collection-calculate-with-ES6/main/reduce/compute_median.js
+++ my-collection-calculate-with-ES6/main/reduce/compute_median.js
@@ -4,7 +4,7 @@ function compute_median(collection) {
   var collection_decs = rank_desc(collection);
   var len = collection_decs.length;
   if (len % 2) {
-    return (collection_decs[Math.ceil(len / 2)]);
+    return (collection_decs[Math.floor(len / 2)]);
   }
   else {
     return (collection_decs[len / 2 - 1] + collection_decs[len / 2]) / 2;
