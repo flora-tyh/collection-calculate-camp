3a90e63b5fd9c1fa1feb6a297206416814792c6b
tyh <756467256@qq.com>
2020-01-07 12:07:13 +0800

    {YunHan}{feat}--{ES6}:finish reduce

--- my-collection-calculate-with-ES6/main/reduce/find_element_first_subscript.js
+++ my-collection-calculate-with-ES6/main/reduce/find_element_first_subscript.js
@@ -1,11 +1,7 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-  for (var i = 0, len = collection.length; i < len; i ++) {
-    if (element === collection[i]) {
-      return i;
-    }
-  }
+  return collection.indexOf(element);
 }
 
 module.exports = calculate_elements_sum;
